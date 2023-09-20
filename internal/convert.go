5ba93d23649543e4da2fe23a079b8144041dcd27
lwch <lwch748@gmail.com>
2023-09-20 17:50:52 +0800

    增加convert逻辑

--- internal/convert.go
+++ internal/convert.go
@@ -3,6 +3,7 @@ package internal
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	imodel "llama2/internal/model"
 	"os"
 	"path/filepath"
@@ -43,4 +44,31 @@ func Convert(*cobra.Command, []string) {
 	logging.Info("saving model to %s...", dir)
 	md.ToScalarType(consts.KBFloat16).Save(dir) // TODO: quantize
 	logging.Info("model saved")
+
+	dir = filepath.Join(OutputDir, "params.json")
+	logging.Info("saving params to %s...", dir)
+	data, err = json.Marshal(params)
+	runtime.Assert(err)
+	runtime.Assert(os.WriteFile(dir, data, 0644))
+	logging.Info("params saved")
+
+	dir = filepath.Join(ModelDir, "tokenizer.model")
+	logging.Info("copying tokenizer from %s...", dir)
+	runtime.Assert(copyFile(dir, filepath.Join(OutputDir, "tokenizer.model")))
+	logging.Info("tokenizer copied")
+}
+
+func copyFile(src, dst string) error {
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+	dstFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer dstFile.Close()
+	_, err = io.Copy(dstFile, srcFile)
+	return err
 }
